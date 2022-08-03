cd21b97fc298033a474a3db4225fb67997b41f88
KeeveRW11 <keevewhyte+github@gmail.com>
2022-08-03 19:38:14 -0400

    dragdrop done

--- assets/js/script.js
+++ assets/js/script.js
@@ -175,7 +175,54 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+ 
+  update: function(event) 
+  { var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function() {
+        tempArr.push({
+          text:$(this)
+          .find("p")
+          .text()
+          .trim(),
 
+         date: $(this)
+          .find("span")
+          .text()
+          .trim()        
+          // add task data to the temp array as an object    
+        });
+     });
+
+  // trim down list's ID to match object property
+  var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+  // update array on tasks object and save
+  tasks[arrName] = tempArr;
+  saveTasks();
+  },
+});
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
@@ -186,6 +233,20 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 // load tasks for the first time
 loadTasks();
 
