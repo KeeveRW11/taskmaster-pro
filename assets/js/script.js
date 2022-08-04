156458cc5dcbe78b9e1190f4faa437c91a275171
KeeveRW11 <keevewhyte+github@gmail.com>
2022-08-03 22:54:54 -0400

    add font

--- assets/js/script.js
+++ assets/js/script.js
@@ -292,4 +292,8 @@ var auditTask = function(taskEl) {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
